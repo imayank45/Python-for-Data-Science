03bb68278a20545e16cd86ad0e6d4a7a4efa2b5f
Mayank Kathane <mkathane84@gmail.com>
2023-07-24 20:01:53 +0530

    first commit

--- OOPs.py
+++ OOPs.py
@@ -18,3 +18,75 @@ c2 = Complex(20,30)
 c2.print()
 c1.add(c2)
 c1.print()
+
+# class and instance variables
+
+class Employee:
+    compName = 'ABC'
+    def __init__(self,id):
+        self.id = id
+e = Employee(101)
+print(e.compName)
+print(e.id)
+print(Employee.compName)
+
+
+class Employee:
+    compName = 'ABC'
+    def __init__(self,id):
+        self.id = id
+e1 = Employee(1011)
+e2 = Employee(1012)
+Employee.compName = 'ABCDEFGHI'
+print(e1.compName)
+print(e2.compName)
+
+
+class Employee:
+    compName = 'ABC'
+    def __init__(self,id):
+        self.id = id
+    def fun(self,name):
+        self.name = name
+e = Employee(103)
+e.fun('Mayank')
+print(e.name)
+e.designation = 'DS'
+print(e.designation)
+
+
+# class member access in pyhton
+
+# 1. In python every member is accessible everywhere
+
+class Test:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+t = Test(11,33)
+print(t.x)
+print(t.y)
+
+# 2. When we use underscore before variable name, we suggest not to use it outside the class
+
+class Test:
+    def __init__(self,x,y):
+        self._x = x
+        self.y = y
+    def _fun(self):
+        print('Hi')
+t1 = Test(10,20)
+print(t1._x)
+print(t1.y)
+
+# 3. When we use two underscores it becomes inaccessible
+
+class Test:
+    def __init__(self,x,y):
+        self.__x = x
+        self.y = y
+    def __fun(self):
+        print('Hi')
+t2 = Test(1,2)
+print(t2.__x)
+print(t2.y)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,10 @@
-d = {101 : 'abc', 102 : 'pqr', 103 : 'xyz'}
-print(d)
-d = { }
-d['laptop'] = 72000
-d['mobile'] = 45000
-d['earphone'] = 2500
-print(d)
-print(d['mobile'])
+
+class Test:
+    def __init__(self,x,y):
+        self.__x = x
+        self.y = y
+    def __fun(self):
+        print('Hi')
+t2 = Test(1,2)
+print(t2.__x)
+print(t2.y)
